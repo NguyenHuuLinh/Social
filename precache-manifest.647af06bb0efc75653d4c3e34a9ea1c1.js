125eaa265f52c61cf6a4196d2eed6a2a5105105b
HuuLinh <huulinh.nguyen@pmax.com.vn>
2019-07-23 09:45:37 +0700

    Updates

---
+++ precache-manifest.647af06bb0efc75653d4c3e34a9ea1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a79135e37d00335c67c55585196cc56",
+    "url": "/Social/index.html"
+  },
+  {
+    "revision": "86f88f444e7ade33c783",
+    "url": "/Social/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c5b836beba7112a493b9",
+    "url": "/Social/static/js/2.2079a4b4.chunk.js"
+  },
+  {
+    "revision": "86f88f444e7ade33c783",
+    "url": "/Social/static/js/main.a08c7931.chunk.js"
+  },
+  {
+    "revision": "d382d6ca6b9e203a48a0",
+    "url": "/Social/static/js/runtime~main.3e47c0be.js"
+  }
+]);

f8988987358a39e6fb6e206f3455781214dc57cf
HuuLinh <huulinh.nguyen@pmax.com.vn>
2019-07-24 13:52:06 +0700

    deploy in heruku

--- src/Layouts/Home/Content/Post/index.js
+++ src/Layouts/Home/Content/Post/index.js
@@ -1,23 +1,71 @@
 import React from 'react'
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoad: false,
+            dataPost: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://nguyenhuulinh.github.io/listPost.json")
+            .then(res => res.json())
+            .then(result => {
+                console.log("render 2");
+                this.setState({
+                    isLoad: true,
+                    dataPost: result.resource_response.data
+                }, () => {
+                    console.log(this.state.dataPost)
+                })
+            })
+    }
+
     render() {
-        return (
-            <div className="bg-gray-100 mt-8 w-4/5 ml-12 pt-1" style={{ "borderRadius": "8px" }}>
-                <div className="bg-gray-900 h-12 w-12 ml-4 mt-2 mb-2" style={{ "borderRadius": "32px" }}>
-                    <div className="ml-16 text-gray-900"> @name</div>
-                    <div className="ml-16 text-gray-600 text-xs">Online</div>
+        const {
+            isLoad,
+            dataPost
+        } = this.state
+
+        // return(
+        //     <div>
+        //         aaa
+        //     </div>
+        // )
+        if (!isLoad) {
+            return (
+                <div>Loading...</div>
+            )
+        } else {
+            return (
+                <div className="flex flex-wrap">
+                    {dataPost && dataPost.map((item, index) => {
+                        return (
+                            <div key={index} className="my-8 w-2/4 px-4 pt-1">
+                                <div className="bg-gray-100 p-3 rounded-lg">
+                                    <div className="bg-gray-900 w-8 ml-4 mt-2 mb-2 rounded-full">
+                                        <div className="ml-16 text-gray-900">{item.domain}</div>
+                                        <div className="ml-16 text-gray-600 text-xs">{item.status}</div>
+                                    </div>
+                                    <p className="text-gray-700 float-left ml-4">
+                                        {item.caption}
+                                    </p>
+                                    <img className="w-full h-64 px-4 py-4" style={{ "borderRadius": "16px" }} src={item.images.orig.url} alt="" />
+                                    <div class="flex mb-4">
+                                        <div class="ion-android-favorite-outline w-1/2 h-12 text-black"><p className="ml-2 inline-block">{index}</p></div>
+                                        <div class="ion-ios-chatbubble-outline w-1/2 h-12 text-black"><p className="ml-2 inline-block">{index + 1}</p></div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    )
+                    }
                 </div>
-                <p className="text-gray-700 float-left ml-4">
-                    This is caption.
-                </p>
-                <img className="w-full h-64 px-4 py-4" style={{ "borderRadius": "16px" }} src="https://pbs.twimg.com/profile_images/895274026783866881/E1G1nNb0.jpg" alt="Sunset in the mountains" />
-                <div className="float-left">
-                    <i className="ion-android-favorite-outline text-gray-700 ml-4 not-italic"><p className="ml-2 inline-block">2</p></i>
-                    <i className="ion-ios-chatbubble-outline text-gray-700 ml-4 not-italic"><p className="ml-2 inline-block">1</p></i>
-                </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
